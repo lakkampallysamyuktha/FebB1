2f7a23c24154c654903d0517b5906d81e9307b2b
lakkampallysamyuktha <lakkampallysamyuktha@gmail.com>
2026-03-11 20:04:22 +0530

    Added React Router folder with nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,49 @@
-import './App.css'
-import ConditionalRendering from './ConditionalRendering';
-import RegistrationForm from './RegistrationForm';
-// import DestructuringProps from "./DestructuringProps";
+import React from "react";
+import "./Router/style.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./Router/Home";
+import About from "./Router/About";
+import Services from "./Router/Services";
+import WebDev from "./Router/WebDev";
+import AppDev from "./Router/AppDev";
+import UIUX from "./Router/UIUX";
+import Contact from "./Router/Contact";
+import Profile from "./Router/Profile";
+import Settings from "./Router/Settings";
+
 
 export default function App() {
   return (
-    <div>
-      <RegistrationForm />
-     
-      {/* <h1 style={{ textAlign: "center" }}>USER DETAILS</h1>
+    <BrowserRouter>
 
-      <table
-        border="1"
-        cellPadding="10"
-        style={{
-          width: "100%",
-          textAlign: "center",
-          borderCollapse: "collapse"
-        }}
-      >
-        <thead style={{ backgroundColor: "#f2f2f2" }}>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>City</th>
-            <th>Qualification</th>
-            <th>Experience</th>
-          </tr>
-        </thead>
+      <div className="header">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/settings">Settings</Link>
+      </div>
 
-        <tbody>
-          <DestructuringProps
-            name="Aadhya"
-            age={25}
-            email="Aadhya@gmail.com"
-            phone="9876543210"
-            city="Chennai"
-            qualification="B.Tech"
-            experience={3}
-          />
+      <Routes>
 
-          <DestructuringProps
-            name="Abhi"
-            age={28}
-            email="Abhi@gmail.com"
-            phone="8765432110"
-            city="Bangalore"
-            qualification="MBA"
-            experience={4}
-          />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
 
-          <DestructuringProps
-            name="srikanth"
-            age={24}
-            email="srikanth@gmail.com"
-            phone="1234567890"
-            city="Hyderabad"
-            qualification="B.Com"
-            experience={2}
-          />
+        {/* Nested Routes */}
+        <Route path="/services" element={<Services />}>
+          <Route path="web" element={<WebDev />} />
+          <Route path="app" element={<AppDev />} />
+          <Route path="uiux" element={<UIUX />} />
+        </Route>
 
-          <DestructuringProps
-            name="Meena"
-            age={30}
-            email="meena@gmail.com"
-            phone="1234567891"
-            city="Bangalore"
-            qualification="MCA"
-            experience={6}
-          />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
 
-          <DestructuringProps
-            name="Varshu"
-            age={35}
-            email="varshu@gmail.com"
-            phone="9865432101"
-            city="Kolkata"
-            qualification="B.Sc"
-            experience={10}
-          />
-        </tbody>
-      </table> */}
-    </div>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
